d8e17333f755df562100bcd735a91703a9a54a29
angelovargas <angelo.vargas@jmc.edu.ph>
2016-02-27 12:04:50 +0800

    PreFinalExam-Vargas

--- PreFinalExam/Form1.cs
+++ PreFinalExam/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace PreFinalExam
 {
@@ -15,5 +17,26 @@ namespace PreFinalExam
         {
             InitializeComponent();
         }
-    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process[] process = Process.GetProcesses();
+
+            foreach (Process prs in process)
+            {
+                listBox1.Items.Add(prs.ProcessName + "(" + prs.PrivateMemorySize64.ToString() + ")");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("SOFTWARE\\PreFinalExam-Vargas");
+            key.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
 }
