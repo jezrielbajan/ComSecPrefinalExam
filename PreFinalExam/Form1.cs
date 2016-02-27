90ac19ad6a662042bb4f5903ebe01f68165dd93d
Sheera Mae Penaranda <Sheera Mae Penaranda>
2016-02-27 12:18:26 -0800

    Pre-Final

--- PreFinalExam/Form1.cs
+++ PreFinalExam/Form1.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Diagnostics;
 namespace PreFinalExam
 {
     public partial class Form1 : Form
@@ -14,6 +14,28 @@ namespace PreFinalExam
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process[] process = Process.GetProcesses();
+
+            foreach (Process prs in process)
+            {
+                listBox1.Items.Add(prs.ProcessName + "(" + prs.PrivateMemorySize64.ToString() + ")");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("SOFTWARE\\Pre-FinalExam-Penaranda");
+            key.Close();
         }
     }
 }
