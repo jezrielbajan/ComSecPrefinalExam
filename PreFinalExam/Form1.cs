b0583a87a144d3965c4acf761bb6d5624d2fcc5b
Martin Paolo Vallente <Martin Paolo Vallente>
2016-02-27 11:56:44 +0800

    lalalla

--- PreFinalExam/Form1.cs
+++ PreFinalExam/Form1.cs
@@ -6,14 +6,41 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace PreFinalExam
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process[] process = Process.GetProcesses();
+
+            foreach (Process prs in process)
+            {
+                listBox1.Items.Add(prs.ProcessName + "         (" + prs.PrivateMemorySize64.ToString() + ")");
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+      
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("SOFTWARE\\Pre-FinalExam-Vallente");
+            key.Close();
         }
     }
 }
